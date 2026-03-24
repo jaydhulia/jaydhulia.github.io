f2ab8ce9a9183e43628026e623bc5437561bcbcd
jaydhulia <jaydhulia@hotmail.com>
2026-03-24 16:58:59 -0700

    updates

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -4,11 +4,13 @@ export const workExperienceData = [
     title: "Senior Cloud Infrastructure Security Engineer",
     date_location: "Jun 2022 - Present | Los Gatos, California, USA \n",
     responsibilities: [
-      "Discovered several critical P0 vulnerabilities that would have allowed for a complete compromise of the Netflix AWS infrastructure, including CVE-2024-5023",
-      "Informally nicknamed Good Find for consistently uncovering critical vulnerabilities and actively contributing to the internal Red Team",
-      "Established and led the vision and strategic direction for AWS developer workflows, enforcing secure guardrails while allowing for developer velocity",
-      "Spearheaded the development of an internal IaC solution and other AWS management tools (such as ConsoleMe, Weep) to achieve the above vision",
-      "Helped develop other Netflix engineers' skills by leading vulnerability walkthrough sessions"
+      [
+        "Identified multiple critical P0 vulnerabilities that could have enabled full compromise of Netflix's AWS infrastructure, including CVE-2024-5023 and CVE-2026-25534",
+        "Earned the informal nickname “Good Find” for consistently uncovering high-impact security issues and contributing to internal Red Team efforts",
+        "Defined and drove the strategic vision for AWS developer workflows, balancing strong security guardrails with high developer velocity",
+        "Led development of internal infrastructure tooling, including an IaC solution and AWS management platforms (e.g., ConsoleMe, Weep), to support secure and scalable operations",
+        "Mentored engineers by leading vulnerability deep-dive sessions, improving organization-wide security awareness and skill development"
+      ]
     ],
     "link": "https://github.com/Netflix/consoleme/security/advisories/GHSA-3783-62vc-jr7x"
   },
